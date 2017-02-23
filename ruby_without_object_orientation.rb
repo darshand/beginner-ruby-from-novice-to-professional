55d7e8729ff3a86c795093603dc41781532d6bf9
darshand <darshan.bd@onze.in>
2017-02-24 00:13:00 +0530

    End of chapter 2

---
+++ ruby_without_object_orientation.rb
@@ -0,0 +1,6 @@
+def say_my_name # No need to have class declaration to write methods
+  puts 'Helloo!! Dharshan here'
+end
+
+# can invoke the method directly after their declaration
+say_my_name
---
+++ string_methods.rb
@@ -0,0 +1,12 @@
+puts "Length of Dharshan is : #{'Dharshan'.length}" # prints length of the string. Where 'My name is Dharshan' is considered as object
+puts "Upcase of Dharshan is : #{'Dharshan'.upcase} " # Convert the string(string object) to all uppercase alphabets
+
+# Other useful string methods
+puts 'dharshan'.capitalize # Dharshan
+puts 'Dharshan'.downcase # dharshan
+puts 'Dharshan'.chop # Dharsha
+puts 'Dharshan'.next # Dharshao
+puts 'Dharshan'.reverse # nahsrahD
+puts 'Dharshan'.sum #
+puts 'Dharshan'.swapcase # dHARSHAN -- We can also chain methods on objects in a same line
+puts 'Dharshan'.upcase.reverse # NAHSRAHD
