c8485dbe4a06d665f3c404145a1add14481ff0e2
darshand <darshan.bd@onze.in>
2017-03-20 20:54:12 +0530

    chapter 4 update

--- chapter_4/analyzer.rb
+++ chapter_4/analyzer.rb
@@ -15,22 +15,17 @@ line_count = lines.size
 text = lines.join
 total_characters = text.length
 total_chars_nospace = text.gsub(/\s+/, '').length # replace space globally
+word_count_with_scan = text.scan(/w+/).length # scan for non alphanumeric chars and count
+word_count_with_split = text.split.length # split with space (default) to count words
+sentence_count = text.split(/\.|\?|!/).count # split with char . and ? and ! symbols(chars)
+paragraph_count = text.split(/\n\n/).length # \n (new line) to distinguish paragraphs
 
 puts "#{line_count} lines"
 puts "#{total_characters} character"
 puts "#{total_chars_nospace} characters (excluding spaces)"
-
-word_count_with_scan = text.scan(/w+/).length # scan for non alphanumeric chars and count
-word_count_with_split = text.split.length # split with space (default) to count words
-
 puts "#{word_count_with_scan} words (from scan)"
 puts "#{word_count_with_split} words (from split)"
-
-sentence_count = text.split(/\.|\?|!/).count # split with char . and ? and ! symbols(chars)
 puts "#{sentence_count} sentences"
-
-paragraph_count = text.split(/\n\n/).length # \n (new line) to distinguish paragraphs
 puts "#{paragraph_count} paragraphs"
-
 puts "#{sentence_count / paragraph_count} sentences per paragraph (average)"
 puts "#{word_count_with_split / sentence_count} words per sentences (average)"
